890fa47872372e0fdf64bfd841c254447b2fd626
sekarasiewicz <sekarasiewicz@gmail.com>
2026-09-02 12:38:33 +0200

    Initial Commit

---
+++ Notchy/NotchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchyApp.swift
+//  Notchy
+//
+//  Created by Sebastian Karasiewicz on 02/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct NotchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
